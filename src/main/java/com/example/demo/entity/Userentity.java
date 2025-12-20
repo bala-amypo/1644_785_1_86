1f3587cf73b3dbd86577f53d307bfeedab1ca2e5
bala-amypo <bala@amypo.in>
2025-12-20 15:09:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Userentity.java
+++ src/main/java/com/example/demo/entity/Userentity.java
@@ -1,3 +1,25 @@
 package com.example.demo.entity;
 
 
+import java.sql.Date;
+
+public class Userentity {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private Date dob;
+    private Date createAt;
+    
+    public Userentity(Date createAt, Date dob, String email, int id, String name, String password) {
+        this.createAt = createAt;
+        this.dob = dob;
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Userentity() {
+}
+}
