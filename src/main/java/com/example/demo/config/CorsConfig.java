c1633e7aa301109cebc632c45123819badf8fec2
bala-amypo <bala@amypo.in>
2025-12-21 09:48:39 +0000

    User commit

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,3 +1,9 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
