29af5c7b468bb20626a4730e46335df510129ba0
bala-amypo <bala@amypo.in>
2025-12-25 16:47:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/CropRepository.java
+++ src/main/java/com/example/demo/repository/CropRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Crop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.*;
 @Repository
 public interface CropRepository extends JpaRepository<Crop, Long> {
     List<Crop> findSuitableCrops(double soilPh, String season);
