19f975d83350b484c9b66523fbdbc660137eb7a3
bala-amypo <bala@amypo.in>
2025-12-25 16:13:43 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,13 +1,13 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spi.DocumentationType;
-import org.springframework.context.annotation.Bean;
+// import springfox.documentation.spring.web.plugins.Docket;
+// import springfox.documentation.spi.DocumentationType;
+// import org.springframework.context.annotation.Bean;
 
-public class SwaggerConfig {
+// public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
-    }
-}
+//     @Bean
+//     public Docket api() {
+//         return new Docket(DocumentationType.SWAGGER_2);
+//     }
+// }
